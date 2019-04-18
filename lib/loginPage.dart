679ab60bd04aebdd20f8f341a2046c2cf6db4a49
Adrian <adriandltd@gmail.com>
2019-04-17 22:39:21 -0500

    Login Buttons Created pt.2

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,5 +1,6 @@
 import 'package:animation_exp/SwipeAnimation/index.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class MyLoginPage extends StatelessWidget {
@@ -53,60 +54,39 @@ class MyLoginPage extends StatelessWidget {
                   height: 50.0,
                   child: RaisedButton(
                     color: Colors.blueGrey,
-                    child:
-                        Text("Log In With Email", style: TextStyle(color: Colors.white)),
+                    child: Text("Log In With Email",
+                        style: TextStyle(color: Colors.white,fontSize: 18)),
                     onPressed: () {
                       loginStuff(context);
                     },
                   )),
               Padding(padding: const EdgeInsets.only(top: 55.0)),
-              ButtonTheme(
-                  minWidth: 325.0,
-                  height: 50.0,
-                  child: RaisedButton(
-                    color: Colors.blue[800],
-                    child:
-                        Text("Log In With Facebook", style: TextStyle(color: Colors.white)),
-                    onPressed: () {
-                      loginStuff(context);
-                    },
-                  )),
+              Container(
+                width: 325,
+                child: GoogleSignInButton(
+                  onPressed: (){
+
+                  },
+                ),
+              ),
               Padding(padding: const EdgeInsets.only(top: 6.0)),
-              ButtonTheme(
-                  minWidth: 325.0,
-                  height: 50.0,
-                  child: RaisedButton(
-                    color: Colors.lightBlue,
-                    child:
-                        Text("Log In With Twitter", style: TextStyle(color: Colors.white)),
-                    onPressed: () {
-                      loginStuff(context);
-                    },
-                  )),
+              Container(
+                width: 325,
+                child: FacebookSignInButton(
+                  onPressed: () {
+
+                  },
+                ),
+              ),
               Padding(padding: const EdgeInsets.only(top: 6.0)),
-              ButtonTheme(
-                  minWidth: 325.0,
-                  height: 50.0,
-                  child: RaisedButton(
-                    color: Colors.red,
-                    child:
-                        Text("Log In With Google", style: TextStyle(color: Colors.white)),
-                    onPressed: () {
-                      loginStuff(context);
-                    },
-                  )),
-                  Padding(padding: const EdgeInsets.only(top: 6.0)),
-              ButtonTheme(
-                  minWidth: 325.0,
-                  height: 50.0,
-                  child: RaisedButton(
-                    color: Colors.green,
-                    child:
-                        Text("Log In With Microsoft", style: TextStyle(color: Colors.white)),
-                    onPressed: () {
-                      loginStuff(context);
-                    },
-                  )),
+              Container(
+                width: 325,
+                child: TwitterSignInButton(
+                  onPressed: () {
+
+                  },
+                ),
+              ),
             ],
           ),
         ],
