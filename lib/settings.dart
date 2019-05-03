a7e2582fd91e99b2ed93fdbfaa29980663d9bc24
Adrian De la Torre <36575300+adriandltd@users.noreply.github.com>
2019-05-03 17:12:27 -0500

    Edit Profile Button in Settings only shows for Created Users not Google, FB, or Twitter

--- lib/settings.dart
+++ lib/settings.dart
@@ -22,10 +22,10 @@ class _SettingsPage extends State<SettingsPage> {
 
   checkforprofilepic() {
     print(profileurl);
-    if (profileurl != null) {
-      return NetworkImage(user.photoUrl);
+    if (profileurl == null) {
+      return AssetImage("assets/profileEmpty.png");
     } else {
-      return AssetImage("assets/findmaxcatchphrase.png");
+      return NetworkImage(user.photoUrl);
     }
   }
 
@@ -39,56 +39,105 @@ class _SettingsPage extends State<SettingsPage> {
     isUserSignedIn = false;
   }
 
+  checkifUserisCreated()
+  {
+    if(user.providerData[1].providerId == "password"){
+          return Container(
+            child: ButtonTheme(
+                minWidth: 275.0,
+                height: 35.0,
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  color: Colors.indigo,
+                  child: Text("Edit Profile",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700)),
+                  onPressed: () {
+                    
+                  },
+                )),
+          );
+    }
+    else
+      return Container();
+  }
   String profileurl;
   FirebaseUser user;
   String displayName;
 
   Widget _buildWidget() {
     checkCredentials();
-    return ListView(children: <Widget>[
-      Column(children: <Widget>[
-        Padding(padding: const EdgeInsets.only(top: 20.0)),
-        Center(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              Container(
-                  width: 120.0,
-                  height: 120.0,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          fit: BoxFit.fill, image: checkforprofilepic())))
-            ])),
-        Padding(padding: const EdgeInsets.only(top: 20.0)),
-        Text(
-          user.displayName,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          radius: 0.80,
+          center: Alignment.center,
+          stops: [.33, .66, .99],
+          colors: [
+            // Color.fromRGBO(255, 212, 109, 1),
+            // Color.fromRGBO(255, 200, 70, 1),
+            // Color.fromRGBO(255, 194, 43, 1),
+            Color.fromRGBO(255, 212, 109, 1), //Yellow
+            Color.fromRGBO(255, 200, 70, 1),
+            Color.fromRGBO(255, 194, 43, 1),
+          ],
         ),
-        Padding(padding: const EdgeInsets.only(top: 20.0)),
-        Container(
-          child: ButtonTheme(
-              minWidth: 275.0,
-              height: 35.0,
-              child: RaisedButton(
-                textColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                color: Colors.red,
-                child: Text("Sign Out",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700)),
-                onPressed: () {
-                  signout();
-                  Navigator.pop(context);
-                },
-              )),
-        ),
+      ),
+      child: ListView(children: <Widget>[
+        Column(children: <Widget>[
+          Padding(padding: const EdgeInsets.only(top: 20.0)),
+          Center(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                Container(
+                    width: 120.0,
+                    height: 120.0,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black87,
+                          width: 1.5,
+                        ),
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            fit: BoxFit.fill, image: checkforprofilepic())))
+              ])),
+          Padding(padding: const EdgeInsets.only(top: 20.0)),
+          Text(
+            user.displayName,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Padding(padding: const EdgeInsets.only(top: 20.0)),
+          checkifUserisCreated(),
+          Padding(padding: const EdgeInsets.only(top: 20.0)),
+          Container(
+            child: ButtonTheme(
+                minWidth: 275.0,
+                height: 35.0,
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  color: Colors.red,
+                  child: Text("Sign Out",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700)),
+                  onPressed: () {
+                    signout();
+                    Navigator.pop(context);
+                  },
+                )),
+          ),
+        ]),
       ]),
-    ]);
+    );
   }
 
   @override
