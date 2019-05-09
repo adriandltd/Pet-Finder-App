f9a7530ff56e1786cddc1b87577d449e22723d3f
Sergio <istickie408@hotmail.com>
2019-05-08 21:27:14 -0500

    Changed picking of image/updated cards

--- lib/SwipeAnimation/detail.dart
+++ lib/SwipeAnimation/detail.dart
@@ -73,7 +73,7 @@ class _DetailPageState extends State<DetailPage> with TickerProviderStateMixin {
       child: new Container(
         width: width.value,
         height: heigth.value,
-        color: const Color.fromRGBO(106, 94, 175, 1.0),
+                    color: Color.fromRGBO(255, 194, 30, 1),
         child: new Hero(
           tag: "img",
           child: new Card(
@@ -240,7 +240,7 @@ class _DetailPageState extends State<DetailPage> with TickerProviderStateMixin {
                       width: 600.0,
                       height: 80.0,
                       decoration: new BoxDecoration(
-                        color: new Color.fromRGBO(121, 114, 173, 1.0),
+                    color: Color.fromRGBO(255, 194, 30, 1),
                       ),
                       alignment: Alignment.center,
                       child: new Row(
--- lib/SwipeAnimation/styles.dart
+++ lib/SwipeAnimation/styles.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 
 DecorationImage image1 = new DecorationImage(
-  image: new ExactAssetImage('assets/img1.jpg'),
+  image: new ExactAssetImage('assets/doggopic.jpg'),
   fit: BoxFit.cover,
 );
 DecorationImage image2 = new DecorationImage(
-  image: new ExactAssetImage('assets/img2.jpg'),
+  image: new ExactAssetImage('assets/doggopic1.jpg'),
   fit: BoxFit.cover,
 );
 
 DecorationImage image3 = new DecorationImage(
-  image: new ExactAssetImage('assets/img3.jpg'),
+  image: new ExactAssetImage('assets/doggopic2.jpg'),
   fit: BoxFit.cover,
 );
 DecorationImage image4 = new DecorationImage(
-  image: new ExactAssetImage('assets/img4.jpg'),
+  image: new ExactAssetImage('assets/doggopic3.jpg'),
   fit: BoxFit.cover,
 );
 DecorationImage image5 = new DecorationImage(
-  image: new ExactAssetImage('assets/img5.jpg'),
+  image: new ExactAssetImage('assets/doggopic4.jpg'),
   fit: BoxFit.cover,
 );
 
